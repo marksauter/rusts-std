3b2d3efd34195737f09d0e83211c2b94ec4267cc
Mark Sauter <dr.funk.mark@gmail.com>
2019-09-15 13:05:15 -0500

    feat(fmt): add Display/Debug traits for use with function format

---
+++ src/fmt.ts
@@ -0,0 +1,104 @@
+export interface Debug {
+  fmt_debug(): string;
+}
+
+export function isDebug(t: any): t is Debug {
+  return typeof t === "object" && t !== null && (t as Debug).fmt_debug !== undefined;
+}
+
+export interface Display {
+  fmt_display(): string;
+}
+
+export function isDisplay(t: any): t is Display {
+  return typeof t === "object" && t !== null && (t as Display).fmt_display !== undefined;
+}
+
+function fmt_builtin(t: any): string {
+  switch (typeof t) {
+    case "object": {
+      if (t === null) {
+        return "null";
+      } else if (t instanceof Array) {
+        let fmt = "[";
+        for (let i = 0; i < t.length; ++i) {
+          fmt += fmt_display(t[i]);
+          if (i !== t.length - 1) {
+            fmt += ",";
+          }
+        }
+        fmt += "]";
+        return fmt;
+      } else {
+        let fmt = "{";
+        for (let p in t) {
+          if (t.hasOwnProperty(p)) {
+            fmt += fmt_display(p);
+            fmt += ":";
+            fmt += fmt_display(t[p]);
+            fmt += ",";
+          }
+        }
+        fmt = fmt.substr(0, fmt.length - 1);
+        fmt += "}";
+        return fmt;
+      }
+    }
+    case "string":
+      return `"${t}"`;
+    case "symbol":
+      return t.toString();
+    default:
+      return `${t}`;
+  }
+}
+
+function fmt_debug(t: any): string {
+  if (isDebug(t)) {
+    return t.fmt_debug();
+  }
+  return fmt_builtin(t);
+}
+
+function fmt_display(t: any): string {
+  if (isDisplay(t)) {
+    return t.fmt_display();
+  }
+  return fmt_builtin(t);
+}
+
+const FORMAT_TEMPLATE_REGEXP = new RegExp(/{([:?]*)}/g);
+
+enum FormatTrait {
+  Display = "",
+  Debug = ":?"
+}
+
+export function format(template: string, ...args: any[]): string {
+  let ret = "";
+  let last_index = 0;
+  let i = 0;
+  let match: ReturnType<typeof RegExp.prototype.exec>;
+  while ((match = FORMAT_TEMPLATE_REGEXP.exec(template)) !== null) {
+    let arg = args[i];
+    let placeholder_index = match.index;
+    let placeholder_length = match[0].length;
+    let trait = match[1];
+    let fmt = "";
+    switch (trait) {
+      case FormatTrait.Display: {
+        fmt = fmt_display(arg);
+        break;
+      }
+      case FormatTrait.Debug: {
+        fmt = fmt_debug(arg);
+        break;
+      }
+    }
+    ret += template.slice(last_index, placeholder_index) + fmt;
+    last_index = placeholder_index + placeholder_length;
+    i++;
+  }
+
+  return ret;
+}
---
+++ test/fmt.test.ts
@@ -0,0 +1,51 @@
+import { assert_eq, should_panic } from "../src/macros.test";
+import { format } from "../src/fmt";
+
+class Point {
+  x: number;
+  y: number;
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+  fmt_debug(): string {
+    return `Point { x: ${this.x}, y: ${this.y} }`;
+  }
+  fmt_display(): string {
+    return `[${this.x},${this.y}]`;
+  }
+}
+
+describe("fmt", () => {
+  test("format", () => {
+    assert_eq(format("{}", undefined), "undefined");
+    assert_eq(format("{}", null), "null");
+    assert_eq(format("{}", 1), "1");
+    assert_eq(format("{}", BigInt(1)), "1");
+    assert_eq(format("{}", "foo"), '"foo"');
+    assert_eq(format("{}", function() {}), "function () { }");
+    assert_eq(format("{}", () => {}), "function () { }");
+    assert_eq(format("{}", Symbol.iterator), "Symbol(Symbol.iterator)");
+    assert_eq(format("{}", [1, 2]), "[1,2]");
+    assert_eq(format("{}", { foo: "bar" }), '{"foo":"bar"}');
+
+    assert_eq(format("{:?}", undefined), "undefined");
+    assert_eq(format("{:?}", null), "null");
+    assert_eq(format("{:?}", 1), "1");
+    assert_eq(format("{:?}", BigInt(1)), "1");
+    assert_eq(format("{:?}", "foo"), '"foo"');
+    assert_eq(format("{:?}", function() {}), "function () { }");
+    assert_eq(format("{:?}", () => {}), "function () { }");
+    assert_eq(format("{:?}", Symbol.iterator), "Symbol(Symbol.iterator)");
+    assert_eq(format("{:?}", [1, 2]), "[1,2]");
+    assert_eq(format("{:?}", { foo: "bar" }), '{"foo":"bar"}');
+
+    assert_eq(format("{}", new Point(1, 2)), "[1,2]");
+    assert_eq(format("{:?}", new Point(1, 2)), "Point { x: 1, y: 2 }");
+
+    assert_eq(
+      format("New Point {} with x = {} and y = {}", new Point(1, 2), 1, 2),
+      "New Point [1,2] with x = 1 and y = 2"
+    );
+  });
+});
